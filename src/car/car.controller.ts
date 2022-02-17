8f5e10723622f591127ec9135d308353aa60c91c
basdawimp <basharoldknight@gmail.com>
2022-02-17 17:13:03 +0000

    cleaned chunk import visuals

--- src/car/car.controller.ts
+++ src/car/car.controller.ts
@@ -1,4 +1,13 @@
-import { Body, Controller, Delete, Get, Param, Post, Put, Query } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
 import { CarDto } from './car.dto';
 import { CarService } from './car.service';
 
