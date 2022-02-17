b7f03bbe5cb28cfb972f1f76831b88965a45cb76
basdawimp <basharoldknight@gmail.com>
2022-02-17 17:12:13 +0000

    Implemented functionallity to controller methods.

--- src/car/car.controller.ts
+++ src/car/car.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put, Query } from '@nestjs/common';
+import { CarDto } from './car.dto';
 import { CarService } from './car.service';
 
 @Controller('car')
@@ -11,18 +12,24 @@ export class CarController {
   }
 
   @Post()
-  public postCar() {
+  public postCar(@Body() car: CarDto) {
+    return this.carService.postCar(car);
   }
 
-  @Get()
-  public getCatById() {
+  @Get(':id')
+  public getCatById(@Param('id') id: number) {
+    return this.carService.getCarById(id);
   }
 
-  @Delete()
-  public deleteCarById() {
+  @Delete(':id')
+  public deleteCarById(@Param('id') id: number) {
+    return this.carService.deleteCarById(id);
   }
 
-  @Put()
-  public putCarById() {
+  @Put(':id')
+  public putCarById(@Param('id') id: number, @Query() query) {
+    const propertyName = query.property_name;
+    const propertyValue = query.property_value;
+    return this.carService.putCarById(id, propertyName, propertyValue);
   }
 }
